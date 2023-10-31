5ae5878396c848af415e64711532f179ea185333
Thomas Soares <ttsoares@orion.ufrgs.br>
2023-10-31 09:10:59 -0300

    Typos -2

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className=" relative w-full h-full">
         <Image
           src="/images/hero.jpg"
-          alt="Bridge"
+          alt="BuilDesign logo"
           fill
           className="bg-black bg-blend-overlay"
         />
@@ -17,7 +17,7 @@ export default function Home() {
       <div className="absolute top-20 left-0 w-full hidden md:block h-[calc(100%-12%)] bg-gradient-to-b from-white to-black mix-blend-multiply"></div>
 
       <div className="absolute top-1/3 left-[5%]">
-        <h1 className="text-2xl md:text-6xl italic font-extrabold">
+        <h1 className="text-2xl md:text-4xl lg:text-6xl italic font-extrabold">
           A estética da sua marca atraindo clientes...
         </h1>
         <p className="text-xs md:text-xl">
