b1273949df76dbb156edc8a287a12d8b3567aea3
Thomas Soares <ttsoares@orion.ufrgs.br>
2023-11-01 18:41:04 -0300

    New help img

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex w-full h-[calc(100%-6%)] flex-col items-start justify-center">
       <div className=" relative w-full h-full">
         <Image
-          src="/images/hero.jpg"
+          src="/images/hero-3.png"
           alt="BuilDesign logo"
           fill
           className="bg-black bg-blend-overlay"
@@ -16,7 +16,7 @@ export default function Home() {
 
       <div className="absolute top-14 left-0 w-full hidden md:block h-[calc(100%-12%)] bg-gradient-to-b from-gray-200 to-black mix-blend-multiply"></div>
 
-      <div className="absolute top-1/3 text-white left-[5%]">
+      <div className="absolute top-[15%] text-white left-[5%]">
         <h1 className="text-2xl md:text-4xl lg:text-6xl italic font-extrabold">
           A estética da sua marca atraindo clientes...
         </h1>
